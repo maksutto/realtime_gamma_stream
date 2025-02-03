b9ef09a47bbc572627c8d7ecfb0a981dacfe2640
maksutto <165839905+maksutto@users.noreply.github.com>
2025-02-04 01:58:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,7 @@ Hello *world!*
 """)
  
 df = pd.read_csv("https://www.dropbox.com/scl/fi/wc92bzd29se8wv4rzam7w/gamma_range_summ_result_test_7dte.csv?rlkey=lf4pwojnlwq91jc1cfyhnuwa9&dl=1")
+df['date'] = pd.to_datetime(df['date'], format='mixed')
+df['date'] = df['date']
+df.index = df['date']
 st.line_chart(df)
