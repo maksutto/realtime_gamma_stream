2d68f25f0abcdde484bee178dc6f7a96258d3895
maksutto <165839905+maksutto@users.noreply.github.com>
2025-02-04 02:33:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ df['date'] = pd.to_datetime(df['date'], format='mixed')
 # st.line_chart(df['zero'],width = 1000, height = 1000)
 st.line_chart(
     df,
-    x = ['date'],
+    x = 'date',
     y = ["zero", "low"],
     color=["#FF0000", "#0000FF"],
     width = 1000, height = 1000  # Optional
