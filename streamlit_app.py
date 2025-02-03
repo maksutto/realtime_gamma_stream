ac1618cfb218430cbede3a7ebebffe4cb1d21366
maksutto <165839905+maksutto@users.noreply.github.com>
2025-02-04 01:56:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ st.write("""
 Hello *world!*
 """)
  
-df = pd.read_csv("my_data.csv")
+df = pd.read_csv("https://www.dropbox.com/scl/fi/wc92bzd29se8wv4rzam7w/gamma_range_summ_result_test_7dte.csv?rlkey=lf4pwojnlwq91jc1cfyhnuwa9&dl=1")
 st.line_chart(df)
