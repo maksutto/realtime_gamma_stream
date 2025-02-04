60ae02a710228e16db87fdde75b0af2295a3dfb5
Max Malyy <165839905+maksutto@users.noreply.github.com>
2025-02-04 10:15:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 import pandas as pd
- 
+import yfinance
 st.write("""
-# My first app
-Hello *world!*
+GAVNA*
 """)
  
-df = pd.read_csv("https://www.dropbox.com/scl/fi/wc92bzd29se8wv4rzam7w/gamma_range_summ_result_test_7dte.csv?rlkey=lf4pwojnlwq91jc1cfyhnuwa9&dl=1")
-df['date'] = pd.to_datetime(df['date'], format='mixed')
+df_7dte = pd.read_csv("https://www.dropbox.com/scl/fi/wc92bzd29se8wv4rzam7w/gamma_range_summ_result_test_7dte.csv?rlkey=lf4pwojnlwq91jc1cfyhnuwa9&dl=1")
+df_7dte['date'] = pd.to_datetime(df_7dte['date'], format='mixed')
 # df.index = df['date']
 # st.line_chart(df['zero'],width = 1000, height = 1000)
 st.line_chart(
-    df,
+    df_7dte,
     x = 'date',
     y = ["zero", "low"],
     color=["#FF0000", "#0000FF"],
