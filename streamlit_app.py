30269396ce1e69a64b78d78b1ba3575bbb4b3729
Max Malyy <165839905+maksutto@users.noreply.github.com>
2025-02-04 10:20:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,11 @@ GAVNA*
  
 df_7dte = pd.read_csv("https://www.dropbox.com/scl/fi/wc92bzd29se8wv4rzam7w/gamma_range_summ_result_test_7dte.csv?rlkey=lf4pwojnlwq91jc1cfyhnuwa9&dl=1")
 df_7dte['date'] = pd.to_datetime(df_7dte['date'], format='mixed')
-# df.index = df['date']
-# st.line_chart(df['zero'],width = 1000, height = 1000)
+df_7dte.index = df_7dte['date']
+vanna_df_7dte = pd.read_csv("https://www.dropbox.com/scl/fi/c3qd9wv4ltbb3t8neobu1/vanna_range_summ_result_test_7dte.csv?rlkey=y2fngkjtqrjhs4rbs8tu8hhgv&dl=1')
+vanna_df_7dte['date'] = pd.to_datetime(vanna_df_7dte['date'], format='mixed')
+vanna_df_7dte.index = vanna_df_7dte['date']   
+
 st.line_chart(
     df_7dte,
     x = 'date',
