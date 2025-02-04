bf539e2ef91498379f6c1d683500e62899efc6ee
Max Malyy <165839905+maksutto@users.noreply.github.com>
2025-02-04 10:34:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,18 @@ GAVNA*
 df_7dte = pd.read_csv("https://www.dropbox.com/scl/fi/wc92bzd29se8wv4rzam7w/gamma_range_summ_result_test_7dte.csv?rlkey=lf4pwojnlwq91jc1cfyhnuwa9&dl=1")
 df_7dte['date'] = pd.to_datetime(df_7dte['date'], format='mixed')
 df_7dte.index = df_7dte['date']
+df_7dte = df_7dte[df_7dte['date'] >= '2025-01-30']
 vanna_df_7dte = pd.read_csv('https://www.dropbox.com/scl/fi/c3qd9wv4ltbb3t8neobu1/vanna_range_summ_result_test_7dte.csv?rlkey=y2fngkjtqrjhs4rbs8tu8hhgv&dl=1')
 vanna_df_7dte['date'] = pd.to_datetime(vanna_df_7dte['date'], format='mixed')
 vanna_df_7dte.index = vanna_df_7dte['date']   
+vanna_df_7dte = vanna_df_7dte[vanna_df_7dte['date'] >= '2025-01-30']
 
-full_df = pd.concat([df_7dte["zero"],df_7dte["low"],vanna_df_7dte["maxVanna"],vanna_df_7dte["minVanna"],vanna_df_7dte["zero"]],keys = ["zero","low","vamma_maxVanna","vamma_minVanna","vamma_zero"],axis = 1)
+full_df = pd.concat([df_7dte["zero"],df_7dte["low"],vanna_df_7dte["maxVanna"],vanna_df_7dte["minVanna"],vanna_df_7dte["zero"]],keys = ["zero","low","vanna_maxVanna","vanna_minVanna","vanna_zero"],axis = 1)
 
 st.line_chart(
     full_df,
     x = None,
-    y = ["zero","low","vamma_maxVanna","vamma_minVanna","vamma_zero"],
+    y = ["zero","low","vanna_maxVanna","vanna_minVanna","vanna_zero"],
     # color=["#FF0000", "#0000FF"],
     width = 2000, height = 1000  # Optional
 )
