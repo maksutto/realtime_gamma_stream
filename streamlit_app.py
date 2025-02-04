cfd53e41e660e3c5c735234127ce1a83daa1f2b8
Max Malyy <165839905+maksutto@users.noreply.github.com>
2025-02-04 10:22:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,12 @@ GAVNA*
 df_7dte = pd.read_csv("https://www.dropbox.com/scl/fi/wc92bzd29se8wv4rzam7w/gamma_range_summ_result_test_7dte.csv?rlkey=lf4pwojnlwq91jc1cfyhnuwa9&dl=1")
 df_7dte['date'] = pd.to_datetime(df_7dte['date'], format='mixed')
 df_7dte.index = df_7dte['date']
-vanna_df_7dte = pd.read_csv("https://www.dropbox.com/scl/fi/c3qd9wv4ltbb3t8neobu1/vanna_range_summ_result_test_7dte.csv?rlkey=y2fngkjtqrjhs4rbs8tu8hhgv&dl=1')
+vanna_df_7dte = pd.read_csv('https://www.dropbox.com/scl/fi/c3qd9wv4ltbb3t8neobu1/vanna_range_summ_result_test_7dte.csv?rlkey=y2fngkjtqrjhs4rbs8tu8hhgv&dl=1')
 vanna_df_7dte['date'] = pd.to_datetime(vanna_df_7dte['date'], format='mixed')
 vanna_df_7dte.index = vanna_df_7dte['date']   
+
+tick = yfinance.Ticker("^SPX")
+hist = tick.history(start=summ_voll["date"].tolist()[0], interval = "1d")
 
 st.line_chart(
     df_7dte,
